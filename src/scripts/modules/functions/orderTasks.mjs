c7e2078bed264862c3138f8f67182b86b4399a32
Chawki <chawkimbarki500@gmail.com>
2023-05-17 18:32:09 +0100

    Add "orderTasks" function

---
+++ src/scripts/modules/functions/orderTasks.mjs
@@ -0,0 +1,13 @@
+export default function orderTasks(tasks) {
+  if (tasks.length > 1) {
+    for (let i = 0; i < tasks.length - 1; i += 1) {
+      for (let j = i+1; j < tasks.length; j += 1) {
+        if(tasks[i]["index"] > tasks[j]["index"]){
+          let swap = tasks[i];
+          tasks[i] = tasks[j];
+          tasks[j] = swap
+        }
+      }
+    }
+  }return tasks;
+}
