417824e45f21693a03ab51fe2a41f3544edc3968
Chawki <chawkimbarki500@gmail.com>
2023-05-19 15:14:05 +0100

    Fix Linter  errors

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,6 @@
 import '../styles/style.css';
 
-import Task from './modules/classes/task.mjs';
 import addNewTask from './modules/functions/addNewTask.mjs';
-import createTaskElement from './modules/functions/createTaskElement.mjs';
 import displayAllTasksElements from './modules/functions/displayAllTasksElements.mjs';
 import displayTaskElement from './modules/functions/displayTaskElement.mjs';
 
@@ -12,27 +10,27 @@ const container = document.querySelector('.container');
 const addedTask = document.querySelector('.add-task input');
 
 let tasks = grabTasks();
-localStorage.setItem("tasks", JSON.stringify(tasks))
+localStorage.setItem('tasks', JSON.stringify(tasks));
 displayAllTasksElements(tasks);
 
 tasks.forEach((task) => {
-  task.element.addEventListener("keyup", () => {
-    localStorage.setItem("tasks", JSON.stringify(tasks))
+  task.element.addEventListener('keyup', () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
   });
-  let taskCheck = task.element.children[0];
-  taskCheck.addEventListener("change", ()=> {
-    localStorage.setItem("tasks", JSON.stringify(tasks))
-  })
-})
+  const taskCheck = task.element.children[0];
+  taskCheck.addEventListener('change', () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  });
+});
 
-addedTask.addEventListener("keypress", (e) => {
-  if(e.key == "Enter") {
-    tasks = addNewTask(tasks, addedTask)
-    localStorage.setItem("tasks", JSON.stringify(tasks))
-    displayTaskElement(tasks[tasks.length - 1])
-    addedTask.value=""
+addedTask.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    tasks = addNewTask(tasks, addedTask);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    displayTaskElement(tasks[tasks.length - 1]);
+    addedTask.value = '';
   }
-})
+});
 
 const btn = document.createElement('button');
 btn.textContent = 'Clear all completed';
