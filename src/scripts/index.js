6d74f108ec8a892bfa1e218731a2920b74e54391
Chawki <chawkimbarki500@gmail.com>
2023-05-19 11:40:23 +0100

    Remove all hardcoded items from the tasks array.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,24 +1,14 @@
 import '../styles/style.css';
 
 import Task from './modules/classes/task.mjs';
-import addTask from './modules/functions/addTask.mjs';
-import orderTasks from './modules/functions/orderTasks.mjs';
+import addNewTask from './modules/functions/addNewTask.mjs';
+import createTaskElement from './modules/functions/createTaskElement.mjs';
+import displayAllTasksElements from './modules/functions/displayAllTasksElements.mjs';
+import displayTaskElement from './modules/functions/displayTaskElement.mjs';
 
-const todos = document.querySelector('.todos');
 
-const task1 = new Task(0, 'Go to the gym', false);
-const task2 = new Task(1, 'Study', false);
-const task3 = new Task(2, 'play soccer', false);
-const task4 = new Task(3, 'kiss wife for the last time', false);
-const task5 = new Task(4, 'Destroy the solar system', false);
 
-let tasks = [task2, task3, task1, task5, task4];
 
-tasks = orderTasks(tasks);
-tasks.forEach((todo) => {
-  addTask(todo);
-});
 
 const btn = document.createElement('button');
 btn.textContent = 'Clear all completed';
-todos.appendChild(btn);
