2f251e8c5056966cfc8d1b8cd4f79d7856a5fcd8
Chawki <chawkimbarki500@gmail.com>
2023-05-21 23:16:06 +0100

    Clear All completed will remove all completed Todos

--- src/scripts/modules/functions/clearAllCompleted.mjs
+++ src/scripts/modules/functions/clearAllCompleted.mjs
@@ -1,4 +1,9 @@
+import orderTodos from "./orderTodos.mjs";
 
+export default function clearAllCompleted(todos) {
+  todos = todos.filter(function(todo) {
+    return !todo.status;
   });
 
+  return orderTodos(todos)
 }
