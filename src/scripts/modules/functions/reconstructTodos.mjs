8f816ccda745c13b5eb495d98775142c4400005b
Chawki <chawkimbarki500@gmail.com>
2023-05-22 09:25:56 +0100

    Cleaner code a minore adjustments

--- src/scripts/modules/functions/reconstructTodos.mjs
+++ src/scripts/modules/functions/reconstructTodos.mjs
@@ -2,11 +2,11 @@ import Todo from "../classes/todo.mjs";
 import createTodoElement from "./createTodoElement.mjs";
 
 export default function reconstructTodos(todos) {
-  let newTodos = [];
-  todos.forEach(todo => {
+
+  let newTodos = todos.map(todo => {
     let todoElement = createTodoElement(todo.description)
-    todo = new Todo(todo.index, todo.description, todo.status, todoElement)
-    newTodos.push(todo);
+    return new Todo(todo.index, todo.description, todo.status, todoElement)
   });
+  
   return newTodos;
 }
