acfc2378d65d9c92a2a32fadbc4561d9bf89f467
Chawki <chawkimbarki500@gmail.com>
2023-05-26 09:17:46 +0100

    Update the 'removeTodo' functions to use switch instead of if statement

--- src/scripts/modules/functions/removeTodo.mjs
+++ src/scripts/modules/functions/removeTodo.mjs
@@ -1,13 +1,16 @@
 import orderTodos from "./orderTodos.mjs";
 
 export default function removeTodo(todos, index) {
-  if(index === todos.length-1) {
-    todos.pop();
-  }else if (index === 0){
-    todos.shift();
-  }else{
-    todos.splice(index, 1);
+  switch (index) {
+    case todos.length-1:
+      todos.pop();
+      break;
+    case 0:
+      todos.shift();
+      break;
+    default:
+      todos.splice(index, 1);
+      break;
   }
-
   return orderTodos(todos);
 }
