2c5200be980216e89d439ccff14d81c1d4634257
sahlminkok <sahlminkok7@gmail.com>
2023-05-26 18:26:31 +0100

    Add clear all completed task

---
+++ src/scripts/modules/tests/clearAllCompleted.test.js
@@ -0,0 +1,12 @@
+import clearAllCompleted from '../functions/clearAllCompleted.mjs';
+
+describe('clear all completed tasks', () => {
+  const tasks = [];
+  const task1 = new Todo(0, 'task1', false, createTodoElement('taks1'));
+  const task2 = new Todo(1, 'task2', true, createTodoElement('taks2'));
+  const task3 = new Todo(2, 'task3', false, createTodoElement('taks3'));
+  const task4 = new Todo(3, 'task4', true, createTodoElement('taks4'));
+  const task5 = new Todo(4, 'task5', false, createTodoElement('taks5'));
+
+  
+});
